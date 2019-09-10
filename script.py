1cd4f41de2c0084eba496b9b2acff98cd35d663a
VIKASH PATHAK <vikashpathak1121@gmail.com>
2019-09-10 10:04:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,6 +7,15 @@ from flask import Flask, request
 from serve import model_loader
 import numpy as np
 
+import io
+import json
+
+from torchvision import models
+import torchvision.transforms as transforms
+from PIL import Image
+from flask import Flask, jsonify, request
+
+
 import re
 #system level operations (like loading files)
 import sys
@@ -28,6 +37,15 @@ photos = UploadSet('photos', IMAGES)
 app.config['UPLOADED_PHOTOS_DEST'] = '.'
 configure_uploads(app, photos)
 
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
 
 @app.route('/')
 def index():
@@ -47,28 +65,37 @@ def upload():
 	#read the image into memory
 	img = image.load_img('./output.png', target_size=(224, 224))
 	#compute a bit-wise inversion so black becomes white and vice versa
-	x = image.img_to_array(img)
-	#make it the right size
-	x = np.expand_dims(x, axis=0)
-	#imshow(x)
-	#convert to a 4D tensor to feed into our model
-	x = preprocess_input(x)
-	print "debug2"
-	#in our computation graph
-	with graph.as_default():
-		#perform the prediction
-		out = model.predict(x)
-		u = decode_predictions(out, top=3)[0]
-		s1 = u[0][1]
-		s2 = u[0][2]*100
-		s3 = u[1][1]
-		s4 = u[1][2]*100
-		s5 = u[2][1]
-		s6 = u[2][2]*100
-		print(s1,s2,s3)
-		print "debug3"
+	
+	tensor = transform_image(img)
+	img=model(tensor)
+	_, predicted = torch.max(img, 1)
+	ans=(int)predicted
+	
+	
+	
+	
+# 	x = image.img_to_array(img)
+# 	#make it the right size
+# 	x = np.expand_dims(x, axis=0)
+# 	#imshow(x)
+# 	#convert to a 4D tensor to feed into our model
+# 	x = preprocess_input(x)
+# 	print "debug2"
+# 	#in our computation graph
+# 	with graph.as_default():
+# 		#perform the prediction
+# 		out = model.predict(x)
+# 		u = decode_predictions(out, top=3)[0]
+    	s1 = ans
+	s2 = ans*100
+# 		s3 = u[1][1]
+# 		s4 = u[1][2]*100
+# 		s5 = u[2][1]
+# 		s6 = u[2][2]*100
+# 		print(s1,s2,s3)
+# 		print "debug3"
 		#convert the response to a string
-		return render_template("index2.html",s1 = s1, s2 = s2, s3 = s3,s4 = s4,s5 = s5,s6 = s6)
+	return render_template("index2.html",s1 = s1, s2 = s2)
 
 
 if __name__ == "__main__":
